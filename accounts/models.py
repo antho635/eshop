5630e2e8f66a7bf9dbe21ca5e58827edfc745b93
toutchykipik <toutchy.kipik@pm.me>
2022-07-16 06:12:06 +0200

    cmt

--- accounts/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-
-
-# AbstractUser
-class Shopper(AbstractUser):
-    pass
--- store/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from store.models import Product, Order, Cart
-
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(Cart)
