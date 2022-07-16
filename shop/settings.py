f4caeba0700aab88728f78cecf4a57edfb09ed4e
toutchykipik <toutchy.kipik@pm.me>
2022-07-16 06:56:04 +0200

    ,N/N

--- shop/settings.py
+++ shop/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
-import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = BASE_DIR / 'templates'
@@ -127,4 +126,4 @@ AUTH_USER_MODEL = "accounts.Shopper"
 # chemin vers dossier static
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
