dee6311f0cc0d61a581ecfc715a7209c8d7a5863
Toutchy kipik <toutchy.kipik@pm.me>
2022-07-23 19:45:54 +0200

    push ecommerce code

---
+++ accounts/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import AbstractUser
+
+
+# AbstractUser
+class Shopper(AbstractUser):
+    pass
+
+
---
+++ store/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
+
+from .models import Product, Cart, Order
+
+
+# index
+def index( request ):
+    products = Product.objects.all()
+    return render(request, 'store/index.html', {'products': products})
+
+
+# product_detail
+def product_detail( request, slug ):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, 'store/detail.html', {'product': product})
+
+
+def add_to_cart( request, slug ):
+    user = request.user
+    product = get_object_or_404(Product, slug=slug)
+    cart, _ = Cart.objects.get_or_create(user=user)
+    order, created = Order.objects.get_or_create(user=user,
+                                                 product=product)
+
+    if created:
+        cart.orders.add(order)
+        cart.save()
+    else:
+        order.quantity += 1
+        order.save()
+
+    return redirect(reverse("product", kwargs={"slug": slug}))
+
+
+def cart( request ):
+    cart = get_object_or_404(Cart, user=request.user)
+
+    return render(request, "store/cart.html", context={"orders": cart.orders.all()})
+
+
+def delete_cart( request ):
+    if cart := request.user.cart:
+        cart.orders.all().delete()
+        cart.delete()
+
+    return redirect("index")
