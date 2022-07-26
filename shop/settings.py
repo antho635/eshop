00fb38f1cd04ea41ee530c6dab87d092f0e94a37
Toutchy kipik <toutchy.kipik@pm.me>
2022-07-26 22:49:48 +0200

    ik

--- shop/settings.py
+++ shop/settings.py
@@ -69,14 +69,16 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
 
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
